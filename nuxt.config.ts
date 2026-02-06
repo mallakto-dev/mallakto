6a96745074c7ef8fac078a5a88377c2939e64a99
sergey-royt <goodstop687@gmail.com>
2026-02-06 20:23:09 +0300

    Add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,11 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'ru',
       },
+      link: [
+        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      ],
     },
   },
   runtimeConfig: {
